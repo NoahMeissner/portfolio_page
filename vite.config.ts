3487ae5326a1912f078df3812c2f87207fc61d31
Noah <108337767+NoahMeissner@users.noreply.github.com>
2026-05-31 20:34:29 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: mode === "production" ? "/portfolio_page/" : "/",
   server: {
     host: "::",
     port: 8080,
